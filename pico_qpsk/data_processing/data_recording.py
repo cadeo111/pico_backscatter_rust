d8d364313bce2c17f0ad231a048855a9fb70cbf7
Caden Keese <github@keese.dev>
2025-02-15 05:33:56 +0100

    added data

--- pico_qpsk/data_processing/data_recording.py
+++ pico_qpsk/data_processing/data_recording.py
@@ -1,4 +1,19 @@
 import uhd
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_samples:int = 20_000_000  # number of samples received
+center_freq = 2460e6 # 2.46GHz
+sample_rate:float = 4e6 # 4MHz
+gain = 50 # dB
 usrp = uhd.usrp.MultiUSRP()
-samples = usrp.recv_num_samps(10000, 100e6, 1e6, [0], 50)
+samples = usrp.recv_num_samps(num_samps=num_samples, freq=center_freq, rate=sample_rate,channels=(0,), gain=50) # units: N, Hz, Hz, list of channel IDs, dB
+np.save("DATA_4mhz.npy", samples)
+
+# print(samples[0:10])
+
+# import uhd
+# usrp = uhd.usrp.MultiUSRP()
+# samples = usrp.recv_num_samps(8e6, 2460e6, 4e6, [0], 50)
 print(samples[0:10])
+
